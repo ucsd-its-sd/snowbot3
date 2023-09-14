7ebc1b8c78cd3138be9bf019a1e0db2044f9c89e
B3DAN <apbailey@ucsd.edu>
2023-09-14 09:41:23 -0700

    Taking RecentFires outside of client on

--- snowbot.js
+++ snowbot.js
@@ -17,15 +17,15 @@ client.on('ready', () => {
 });
 
 
+//0-10            Aedan                 Kevin                Ivan                 Mingson              Jeremy               Bryce                Kelly                Tae                  Vincent
+const IDArray = ['299020647198228480','419970850519777280','283414458590822401','266023244539232257','298696268920913924','147954160526950411','718326776165826580','687482247955218483','136447120658923520',
+//Rolando            Lola
+'96402723435446272','472305444518494208'];
+
+var RecentFires = [];
+
 // Actually do stuff.
 client.on('messageCreate', msg => {
-
-  //0-10            Aedan                 Kevin                Ivan                 Mingson              Jeremy               Bryce                Kelly                Tae                  Vincent
-  const IDArray = ['299020647198228480','419970850519777280','283414458590822401','266023244539232257','298696268920913924','147954160526950411','718326776165826580','687482247955218483','136447120658923520',
-  //Rolando            Lola
-  '96402723435446272','472305444518494208'];
-
-  var RecentFires = [];
   
   var index = msg.content.toLowerCase().indexOf("!cs");
   if (index >= 0 && !isNaN(msg.content.substring(3+index,10+index))){
