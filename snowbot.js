95076fe02e5c2e2af567aee166de525b83044a84
B3DAN <apbailey@ucsd.edu>
2023-09-14 09:29:24 -0700

    Bug Correction Hired

--- snowbot.js
+++ snowbot.js
@@ -20,12 +20,12 @@ client.on('ready', () => {
 // Actually do stuff.
 client.on('messageCreate', msg => {
 
-  //0-10     Aedan                 Kevin                Ivan                 Mingson              Jeremy               Bryce                Kelly                Tae                  Vincent
+  //0-10            Aedan                 Kevin                Ivan                 Mingson              Jeremy               Bryce                Kelly                Tae                  Vincent
   const IDArray = ['299020647198228480','419970850519777280','283414458590822401','266023244539232257','298696268920913924','147954160526950411','718326776165826580','687482247955218483','136447120658923520',
   //Rolando            Lola
   '96402723435446272','472305444518494208'];
 
-  const RecentFires = []
+  const RecentFires = [];
   
   var index = msg.content.toLowerCase().indexOf("!cs");
   if (index >= 0 && !isNaN(msg.content.substring(3+index,10+index))){
@@ -80,20 +80,22 @@ client.on('messageCreate', msg => {
       else{
       toFire = getRandomInt(0,10);
       msg.channel.send(`<@${IDArray[toFire]}> Fired`);
-      RecentFires.push(IDArray[toFire])
+      RecentFires.push(IDArray[toFire]);
+      console.log(RecentFires);
 		  }
     }
     
     if(msg.content.includes("!hired")){
-      if(RecentFires.length = 0){
+      if(RecentFires.length === 0){
         toHire = getRandomInt(0,10);
-        msg.channel.send(`<@${IDArray[toHire]}> Fired!`)
+        msg.channel.send(`<@${IDArray[toHire]}> Fired!`);
         msg.channel.send(`<@${IDArray[toHire]}> And Rehired!`);
       }
       //Pull from recent fired
       else{
-        toHire = RecentFires.pop()
-        msg.channel.send(`<@${IDArray[toHire]}> has been hired! All your shifts start at 6:45AM`)
+        toHire = RecentFires.pop();
+        console.log(toHire);
+        msg.channel.send(`<@${IDArray[toHire]}> has been hired! All your shifts start at 6:45AM`);
       }
     }
 
