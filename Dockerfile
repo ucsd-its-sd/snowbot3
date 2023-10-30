cb9ed07bcb0ea43c9df99c2e0144c8f69335d688
Kevin <kfish610@gmail.com>
2023-10-30 14:49:27 -0700

    Changed to using pure docker so we can pull git

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ ARG NODE_VERSION=20.9.0
 
 FROM node:${NODE_VERSION}-alpine
 
+WORKDIR /usr/snowbot
+
 # Download dependencies as a separate step to take advantage of Docker's caching.
 # Leverage a cache mount to /root/.npm to speed up subsequent builds.
 # Leverage a bind mounts to package.json and package-lock.json to avoid having to copy them into
@@ -13,13 +15,14 @@ RUN --mount=type=bind,source=package.json,target=package.json \
     --mount=type=cache,target=/root/.npm \
     npm ci
 
-# Run the application as a non-root user.
-USER node
-
 # Copy the rest of the source files into the image.
 COPY . .
 
+# Build the application.
 RUN ["npm", "run", "build"]
+
+# Run the application as a non-root user.
+USER node
 
 # Run the application.
 CMD ["npm", "run", "start"]
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,20 @@
-import { readFile } from 'fs/promises'
-import { Client, GatewayIntentBits } from 'discord.js'
+import * as process from "process";
+import { Client, GatewayIntentBits } from "discord.js";
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
-readFile('/run/secrets/config', 'utf-8')
-    .then(JSON.parse)
-    .then(data => client.login(data.token));
+client.login(process.env["DISCORD_TOKEN"]);
 
-client.on('ready', () => {
-    console.log(`Successfully logged in as ${client.user!.tag}`)
+client.on("ready", () => {
+    console.log(`Successfully logged in as ${client.user!.tag}`);
 });
 
-client.on('messageCreate', msg => {
+client.on("messageCreate", (msg) => {
     console.log(msg);
 });
