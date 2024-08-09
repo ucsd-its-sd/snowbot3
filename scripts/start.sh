a39644d3a07943a6e9147a61a2ce1f6e84586460
Kevin Fisher <kfish610@gmail.com>
2024-08-09 14:43:13 -0700

    Update script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
-docker container rm snowbot3
+docker container stop snowbot3
 docker run -d --rm --name=snowbot3 --log-driver=journald -v ./config:/home/snowbot/config ghcr.io/ucsd-its-sd/snowbot3:latest
