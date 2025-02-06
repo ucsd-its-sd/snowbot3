91df875e3776a9ba82c6c9cc0aa8cdd8d09d46c4
Kevin Fisher <kfish610@gmail.com>
2025-02-06 08:35:16 -0800

    Actually add the remove wheniwork command

--- src/commands/wheniwork/index.ts
+++ src/commands/wheniwork/index.ts
@@ -1,8 +1,16 @@
 import { CommandModule } from "../../lib/command";
-import { ListWhenIWorkCommand, RegisterWhenIWorkCommand } from "./wiw";
+import {
+  ListWhenIWorkCommand,
+  RegisterWhenIWorkCommand,
+  RemoveWhenIWorkCommand,
+} from "./wiw";
 
 export class WhenIWorkCommandModule extends CommandModule {
-  commands = [new RegisterWhenIWorkCommand(), new ListWhenIWorkCommand()];
+  commands = [
+    new RegisterWhenIWorkCommand(),
+    new RemoveWhenIWorkCommand(),
+    new ListWhenIWorkCommand(),
+  ];
 
   helpCommand = "!wiw help";
   helpTitle = "When I Work Help";
